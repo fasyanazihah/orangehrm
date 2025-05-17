7d157b384460dfc6669f93c23a14b142a365e0ea
Fasya Nazihah <fasyanazihah0015@gmail.com>
2025-05-17 20:26:10 +0700

    Adjust Timeout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add('selector', (name) => {
-  	return cy.selector(name,{timeout:3600000})
+  	return cy.selector(name,{timeout:600000})
 })
 
 Cypress.Commands.add('call', (name) => {
-  	return cy.wait(name,{timeout:3600000})
+  	return cy.wait(name,{timeout:600000})
 })
